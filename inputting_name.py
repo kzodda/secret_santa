182b2412cce07191475ecd5c1ba15628697e1685
Karl Zodda <kzodda@mac.com>
2020-12-17 14:17:20 -0500

    Changing back to a user input list

--- inputting_name.py
+++ inputting_name.py
@@ -4,8 +4,7 @@ The code to input user names'''
 
 
 def names():
-    #names = input('Please enter in unique names seperated by commas:')
-    names = 'Charles,John,Karl,Mike'
+    names = input('Please enter in unique names seperated by commas:')
     people_to_pick = list(names.split(','))
     people_tobe_picked = people_to_pick.copy()
     return people_to_pick, people_tobe_picked
